07c9fb83afed7324e41ba7a5b171f6a7b6d3d321
vvivchar-bit <v.vivchar@istu.edu.ua>
2026-04-03 12:43:08 +0300

    Add files via upload

---
+++ Тема 8.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+class Singleton {
+private:
+    static Singleton* p_instance;
+
+    Singleton() {
+        cout << "Singleton constructor" << endl;
+    }
+
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
+public:
+    static Singleton* getInstance() {
+        if (!p_instance) {
+            p_instance = new Singleton();
+        }
+        return p_instance;
+    }
+
+    void showMessage() {
+        cout << "This is the only instance" << endl;
+    }
+};
+
+Singleton* Singleton::p_instance = nullptr;
+
+int main() {
+    Singleton* s1 = Singleton::getInstance();
+    Singleton* s2 = Singleton::getInstance();
+
+    s1->showMessage();
+    s2->showMessage();
+
+    cout << "Address of s1: " << s1 << endl;
+    cout << "Address of s2: " << s2 << endl;
+
+    if (s1 == s2) {
+        cout << "Same object" << endl;
+    }
+
+    delete s1;
+    return 0;
+}
---
+++ Тема8.1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+class Singleton {
+private:
+    Singleton() {
+        cout << "Singleton constructor" << endl;
+    }
+
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
+public:
+    static Singleton& getInstance() {
+        static Singleton instance;
+        return instance;
+    }
+
+    void showMessage() {
+        cout << "Meyers Singleton works" << endl;
+    }
+};
+
+int main() {
+    Singleton& s1 = Singleton::getInstance();
+    Singleton& s2 = Singleton::getInstance();
+
+    s1.showMessage();
+    s2.showMessage();
+
+    cout << "Address of s1: " << &s1 << endl;
+    cout << "Address of s2: " << &s2 << endl;
+
+    if (&s1 == &s2) {
+        cout << "Same object" << endl;
+    }
+
+    return 0;
+}
